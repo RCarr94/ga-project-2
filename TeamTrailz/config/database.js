808c16f554d776c0c91ee1b66c9031c1b7dfae1e
Ryan Carr <ryan.g.carr@hotmail.com>
2023-02-05 20:43:05 -0500

    added hosted DB

--- TeamTrailz/config/database.js
+++ TeamTrailz/config/database.js
@@ -1,6 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/trails", {
+mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
   //   useCreateIndex: true,
   useUnifiedTopology: true,
@@ -9,6 +9,6 @@ mongoose.connect("mongodb://localhost/trails", {
 // shortcut to mongoose.connection object
 const db = mongoose.connection;
 
-db.on("connected", function () {
+db.on('connected', function () {
   console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
 });
